96f4ab403ad301df5fdde96a37fd90083b8f932e
anubhav <anubhav15129@iiitd.ac.in>
2016-11-06 05:07:42 +0530

    Fifth commit

--- mainClass.java
+++ mainClass.java
@@ -1,9 +1,11 @@
-#import java.util.*;
-
+import java.util.*;
+import java.swings.*;
 class mainClass{
 	static Scanner s;	
 	public static void main(String[] args){
 		s = new Scanner(System.in);
+	
+
 	}
 
 }
