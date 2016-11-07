bd81103d579df3fa53880978fe57754311b0db9b
anubhav <anubhav15129@iiitd.ac.in>
2016-11-07 22:33:09 +0530

    seventh commit

---
+++ parse_xml.java
@@ -0,0 +1,22 @@
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ReadXMLFile {
+
+   public static void main(String argv[]) {
+
+    try {
+
+	SAXParserFactory factory = SAXParserFactory.newInstance();
+	SAXParser saxParser = factory.newSAXParser();
+
+	DefaultHandler handler = new DefaultHandler() {
+
+	boolean bfname = false;
+	boolean blname = false;
+	boolean bnname = false;
+	boolean bsalary = false;
+
