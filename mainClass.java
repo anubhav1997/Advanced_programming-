f4cd54b2d02814f95c5d3bed04eb56fa46838f75
anubhav <anubhav15129@iiitd.ac.in>
2016-11-11 18:38:44 +0530

    13th commit

--- mainClass.java
+++ mainClass.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.swings.*;
 class mainClass{
-	static Scanner s;	
+	public static Scanner s;	
 	public static void main(String[] args){
 		s = new Scanner(System.in);
 		parse_xml getdata = new parse_xml();
